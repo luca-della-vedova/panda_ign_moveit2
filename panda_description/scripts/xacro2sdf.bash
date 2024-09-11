da970723a2a19aabfea4b8d774a0d11a4a38fa00
Addisu Z. Taddese <addisu@openrobotics.org>
2024-09-11 00:35:46 -0500

    Make it work when not using symlink install
    Signed-off-by: Addisu Z. Taddese <addisu@openrobotics.org>

--- panda_description/scripts/xacro2sdf.bash
+++ panda_description/scripts/xacro2sdf.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # This script converts xacro (URDF variant) into SDF for `panda_description` package
 
-SCRIPT_DIR="$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)"
-XACRO_PATH="$(dirname "${SCRIPT_DIR}")/urdf/panda.urdf.xacro"
-SDF_PATH="$(dirname "${SCRIPT_DIR}")/panda/model.sdf"
+PKG_NAME="panda_description"
+XACRO_PATH="$(ros2 pkg prefix --share $PKG_NAME)/urdf/panda.urdf.xacro"
+SDF_PATH="$(ros2 pkg prefix --share $PKG_NAME)/panda/model.sdf"
 
 # Arguments for xacro
 XACRO_ARGS=(
@@ -17,9 +17,6 @@ XACRO_ARGS=(
     gazebo_preserve_fixed_joint:=false
 )
 
-# Remove old SDF file
-rm "${SDF_PATH}" 2>/dev/null
-
 # Process xacro into URDF, then convert URDF to SDF and edit the SDF to use relative paths for meshes
-"${SCRIPT_DIR}/xacro2sdf_direct.bash" "${XACRO_PATH}" "${XACRO_ARGS[@]}" "${@:1}" >"${SDF_PATH}" &&
+ros2 run $PKG_NAME xacro2sdf_direct.bash "${XACRO_PATH}" "${XACRO_ARGS[@]}" "${@:1}" >"${SDF_PATH}" &&
 echo "Created new ${SDF_PATH}"
